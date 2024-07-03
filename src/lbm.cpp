39420434633d702f32b44c3e2fff7c030b1298bf
italonicacio <italonicacio@gmail.com>
2024-07-03 00:11:58 -0300

    collision implemented

--- src/lbm.cpp
+++ src/lbm.cpp
@@ -4,6 +4,8 @@
 #include <cinttypes>
 #include <stdfloat>
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 
 enum class VelSet {D2Q9, D3Q19};
@@ -82,10 +84,100 @@ constexpr auto GetPopWeights() {
     } 
 }
 
-//em 89 std::float32_t não funciona
+//em 89 std::float32_t não funciona quando tenho que definir template do array ...
 
 constexpr auto pop_dir = GetPopDir<std::int8_t>();
 constexpr auto pop_weights = GetPopWeights<float>();
+
+constexpr std::float32_t cs2 = 1.0 / 3.0;
+constexpr std::float32_t tau = 0.9;
+
+constexpr std::array<std::uint32_t, dim> domain_size = {32, 32};
+
+// o idx em zig é usize, que pode não ser necessariamente um uint64
+constexpr std::array<std::uint32_t, dim> idx2pos(std::uint32_t idx) {
+    if constexpr (dim == 2) {
+        return {idx % domain_size[0], idx / domain_size[0]};
+    } else {
+        return { idx % domain_size[0], (idx / domain_size[0]) % domain_size[1], idx % (domain_size[0] * domain_size[1]) };
+    }
+}
+
+constexpr std::uint32_t pos2idx(std::array<std::uint32_t, dim> pos) {
+    if constexpr (dim == 2) {
+        return pos[0] + pos[1] * domain_size[0];
+    } else {
+        return pos[0] + domain_size[0] * (pos[1] + pos[2] * domain_size[1]);
+    }
+}
+
+// template<typename T>
+// T DotProd(T& x, T& y) {
+    
+//     return std::inner_product(x.begin(), x.end(), y.begin(), static_cast<T>(0));
+// }
+
+template<typename T, typename V>
+T DotProd(T& x, V& y) {
+    
+    return std::inner_product(x.begin(), x.end(), y.begin(), static_cast<T>(0));
+}
+
+std::float32_t FuncFeq(std::float32_t rho, std::array<std::float32_t, dim>& u, std::uint32_t i) {
+    const std::float32_t uc = DotProd(u, pop_dir[i]);
+    const std::float32_t uu = DotProd(u, pop_dir[i]);
+
+    return rho * pop_weights[i] * (1 + uc / cs2 + (uc * uc) / (2 * cs2 * cs2) - (uu) / (2 * cs2));
+
+}
+
+void macroscopics (
+    std::vector<std::array<float, n_pop>>& pop_arr,
+    std::vector<float>& rho_arr,
+    std::vector<std::array<float, dim>> u_arr
+) {
+    std::size_t i = 0;
+    
+    while(i < pop_arr.size()) {
+        const auto pop = pop_arr[i];
+        float rho = 0;
+        for(auto& p : pop) {
+            rho += p;
+        }
+
+        std::array<float, dim> u = {0};
+        for(std::uint32_t j = 0; j < pop.size(); ++j) {
+            auto p = pop[j];
+            for(std::uint32_t d = 0; d < dim; ++d) {
+                // duvida pq em zig é u[d] += p * pop_dir[j]
+                // sendo que pop_dir[j] é um array de 2 dimensões
+                u[d] += p * pop_dir[j][d]; 
+            }
+            rho_arr[i] = rho;
+            u_arr[i] = u;
+        }
+        ++i;
+    }
+}
+
+void Collision(
+    std::vector<std::array<float, n_pop>>& pop_arr,
+    std::vector<float>& rho_arr,
+    std::vector<std::array<float, dim>> u_arr
+) {
+    for(std::uint32_t idx = 0; idx < pop_arr.size(); ++idx) {
+        const float rho = rho_arr[idx];
+        auto u = u_arr[idx];
+        const auto& pop = pop_arr[idx];
+
+        for(std::uint32_t i = 0; i < pop.size(); ++i) {
+            const float f = pop[i];
+            const float feq = FuncFeq(rho, u, i);
+            const float f_coll = (f - feq) / tau;
+            pop_arr[idx][i] = f_coll;
+        }
+    }
+}
 
 int32_t main() {
     
